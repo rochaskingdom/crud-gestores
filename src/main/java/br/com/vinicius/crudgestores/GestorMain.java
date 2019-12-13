17bb9a1c60695b02ddfab13cdfaf1d750f99f4a3
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-13 17:29:05 -0300

    Crud completo para deploy no Heroku

---
+++ src/main/java/br/com/vinicius/crudgestores/GestorMain.java
@@ -0,0 +1,12 @@
+package br.com.vinicius.crudgestores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorMain.class, args);
+	}
+}
